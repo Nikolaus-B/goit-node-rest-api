f2eeffe47e8935309b237564b3098996ea2e30b8
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2024-01-19 00:00:51 +0200

    finish

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,73 @@
-import contactsService from "../services/contactsServices.js";
+import contacts from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import { createContactSchema } from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getContactById = (req, res) => {};
+export const getContactById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.getContactById(id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400);
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400);
+    }
 
-export const updateContact = (req, res) => {};
+    const { id } = req.params;
+    const result = await contacts.updateById(id, req.body);
+
+    if (!result) {
+      throw HttpError(404);
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.removeContact(id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json({
+      message: "Contact deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
