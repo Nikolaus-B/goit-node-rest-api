fec5a1763f9586f563b4b36a813b8623cd0de3c9
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2024-02-15 22:01:22 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -37,7 +37,7 @@ const userSchema = new Schema(
     verificationToken: {
       type: String,
       default: "",
-      // required: [true, "Verify token is required"],
+      required: [true, "Verify token is required"],
     },
   },
   { versionKey: false }
